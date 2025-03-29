025cd46ada023775fe3abe7f1ec92abd35bb15c3
Kim Junseung <rla005@naver.com>
2025-03-29 19:26:26 +0900

    명시적 자료형 변환(캐스팅) + 추가코드 업로드

--- Personal_files/Explicit conversion.cpp
+++ Personal_files/Explicit conversion.cpp
@@ -10,6 +10,12 @@ int main()
 	cout << "Without casting:" << x + y << endl;
 	// Forcing explicit conversion 
 	cout << "With casting:" << static_cast <int> (x) + y;
-	return 0;
 
+	double x2 = 23.56;
+	int y2 = 30;
+	// Allowing implicit conversion 
+	cout << "Without casting:" << x2 + y2 << endl;
+	// Forcing explicit conversion 
+	cout << "With casting:" << static_cast <int> (x2) + y2;
+	return 0;
 }
