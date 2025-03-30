49c841cd9e8e0f2aa602473b695ea81b2bc55623
Kim Junseung <rla005@naver.com>
2025-03-30 15:16:44 +0900

    double 자료형의 오버플로우와 언더플로우 예제

---
+++ Personal_files/Overflow_and_underflow_in_doubles.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <limits>
+using namespace std;
+
+int main() {
+
+	double num1 = +numeric_limits <double> ::max();
+	double num2 = -numeric_limits <double> ::max();
+
+	cout << "The value of maximum double:" << num1 << endl;
+	cout << "The value of minimum double:" << num2 << endl;
+
+	num1 *= 1000.00;
+	num2 *= 1000.00;
+	
+	cout << "The value of num2 * 1000 after underflow:" << num2 << endl;
+
+	return 0;
+}
